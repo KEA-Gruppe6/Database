4a3011e03cd2d0b38e2d718b5d2c8700a311b24f
Bressendorff <frederik@bressendorff.eu>
2024-12-07 13:34:29 +0100

    Initial Neo4j services

---
+++ Program.cs
@@ -0,0 +1,86 @@
+using Database_project.Core;
+using Database_project.Core.Interfaces;
+using Database_project.Core.Services;
+using Database_project.Services;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Neo4j.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Database context
+var connectionString = builder.Configuration.GetConnectionString("MSSQL") ?? Environment.GetEnvironmentVariable("MSSQL");
+builder.Services.AddDbContextFactory<DatabaseContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IAirlineService, AirlineService>();
+builder.Services.AddScoped<IAirportService, AirportService>();
+builder.Services.AddScoped<PlaneService>();
+builder.Services.AddScoped<OrderService>();
+
+builder.Services.AddSingleton(GraphDatabase.Driver(
+            "bolt://localhost:7687",
+            AuthTokens.Basic(
+                "neo4j",
+                "Password123"
+            )
+        ));
+builder.Services.AddScoped<Database_project.Neo4j.Services.AirlineService>();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    var retryCount = 5;
+    var delay = TimeSpan.FromSeconds(10);
+
+    for (int i = 0; i < retryCount; i++)
+    {
+        try
+        {
+            dbContext.Database.Migrate();
+            break;
+        }
+        catch (SqlException)
+        {
+            if (i == retryCount - 1) throw;
+            Thread.Sleep(delay);
+        }
+    }
+
+    //// Add passport length trigger
+    //var sqlFilePath = "passportlengthtrigger.sql";
+    //var sqlQuery = File.ReadAllText(sqlFilePath);
+    //dbContext.Database.ExecuteSqlRaw("IF OBJECT_ID('trg_ValidatePassportNumber', 'TR') IS NOT NULL DROP TRIGGER trg_ValidatePassportNumber;");
+    //dbContext.Database.ExecuteSqlRaw(sqlQuery);
+
+    //// Run seed SQL query
+    //sqlFilePath = "populatedb.sql";
+    //sqlQuery = File.ReadAllText(sqlFilePath);
+    //dbContext.Database.ExecuteSqlRaw(sqlQuery);
+}
+
+
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
