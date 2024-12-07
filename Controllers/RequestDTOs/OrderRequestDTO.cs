bf954b3ba90d9468caa37063c01f87823398a96f
CasperRiis <7541911+CasperRiis@users.noreply.github.com>
2024-12-07 15:51:13 +0100

    OrderRequestDTO fix

--- Controllers/RequestDTOs/OrderRequestDTO.cs
+++ Controllers/RequestDTOs/OrderRequestDTO.cs
@@ -5,6 +5,5 @@ namespace Database_project.Controllers.RequestDTOs
     public class OrderRequestDTO
     {
         public string AirlineConfirmationNumber { get; set; }
-        public Ticket[] Tickets { get; set; }
     }
 }
